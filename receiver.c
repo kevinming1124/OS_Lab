c73d2000333111676ddde6cbe5a14661f6b8ee70
kevinming1124 <kevinming1124@gmail.com>
2024-10-25 02:08:51 +0800

    first

---
+++ receiver.c
@@ -0,0 +1,68 @@
+#include "receiver.h"
+
+void receive(message_t* message_ptr, mailbox_t* mailbox_ptr){
+    /*  TODO: 
+        1. Use flag to determine the communication method
+        2. According to the communication method, receive the message
+    */
+    if(mailbox_ptr->flag == 1){
+        // Message Passing
+        mqd_t mq = mq_open("msgQ", O_CREAT | O_RDONLY, 0666, NULL);
+        mq_receive(mq, message_ptr->message, 1024, NULL);
+        if(strcmp(message_ptr->message, "EOF") == 0){
+            printf("Sender exit!\n");
+        }else{
+            printf("Receiving message: %s\n", message_ptr->message);
+        }
+    }else if(mailbox_ptr->flag == 2){
+        // Shared Memory
+        char *name = "OS";
+        int SIZE = 1024;
+        shm_fd = shm_open(name, O_RDONLY, 0666);
+        ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+        if(strcmp((char *)ptr, "EOF") == 0){
+            printf("Sender exit!\n");
+        }else{
+            printf("Receiving message: %s\n", (char *)ptr);
+        }
+        
+        shm_unlink(name);
+    }
+}
+
+int main(){
+    /*  TODO: 
+        1) Call receive(&message, &mailbox) according to the flow in slide 4
+        2) Measure the total receiving time
+        3) Get the mechanism from command line arguments
+            • e.g. ./receiver 1
+        4) Print information on the console according to the output format
+        5) If the exit message is received, print the total receiving time and terminate the receiver.c
+    */
+    sem_t *sem_send = sem_open("sender", 0);
+    sem_t *sem_recv = sem_open("receiver", 0);
+    struct timespec start, end;
+    double time_taken = 0;
+    message_t message;
+    mailbox_t mailbox;
+    mailbox.flag = atoi(argv[1]);
+    if(mailbox.flag == 1){
+        printf("Message Passing\n");
+    }else if(mailbox.flag == 2){
+        printf("Shared Memory\n");
+    }
+    while(1){
+        sem_wait(sem_send);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        receive(message, &mailbox);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        sem_post(sem_recv);
+        time_taken += (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9;
+    }
+
+    sem_close(sem_send);
+    sem_close(sem_recv);
+
+
+
+}
---
+++ sender.c
@@ -0,0 +1,77 @@
+#include "sender.h"
+void send(message_t message, mailbox_t* mailbox_ptr){
+    /*  TODO: 
+        1. Use flag to determine the communication method
+        2. According to the communication method, send the message
+    */
+    char *name = "OS";
+    int SIZE = 1024;
+    if(mailbox_ptr->flag == 1){
+        // Message Passing
+        mqd_t mq = mq_open("msgQ", O_CREAT | O_WRONLY, 0666, NULL);
+        mq_send(mq, message.message, strlen(message.message), 0);
+    }else if(mailbox_ptr->flag == 2){
+        // Shared Memory
+        shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+        ftruncate(shm_fd, SIZE);
+        ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+        sprintf(ptr, "%s", message.message);
+        ptr += strlen(message.message);
+    }
+}
+
+
+
+int main(){
+    /*  TODO: 
+        1) Call send(message, &mailbox) according to the flow in slide 4
+        2) Measure the total sending time
+        3) Get the mechanism and the input file from command line arguments
+            • e.g. ./sender 1 input.txt
+                    (1 for Message Passing, 2 for Shared Memory)
+        4) Get the messages to be sent from the input file
+        5) Print information on the console according to the output format
+        6) If the message form the input file is EOF, send an exit message to the receiver.c
+        7) Print the total sending time and terminate the sender.c
+    */
+    sem_t *sem_send = sem_open("sender", 0);
+    sem_t *sem_recv = sem_open("receiver", 1);
+    struct timespec start, end;
+    double time_taken = 0;
+    message_t message;
+    mailbox_t mailbox;
+    mailbox.flag = atoi(argv[1]);
+    FILE *file = fopen(argv[2], "r");
+    if(file == NULL){
+        printf("File not found\n");
+        return 0;
+    }
+    if(mailbox.flag == 1){
+        printf("Message Passing\n");
+    }else if(mailbox.flag == 2){
+        printf("Shared Memory\n");
+    }
+    while(fgets(message.message, 100, file) != NULL){
+        if(strcmp(message.message, "EOF") != 0)
+            message.message[strlen(message.message) - 1] = '\0';
+        sem_wait(sem_recv);
+        clock_gettime(CLOCK_MONOTONIC, &start);
+        send(message, &mailbox);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        sem_post(sem_send);
+        time_taken += (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9;
+        if(strcmp(message.message, "EOF") != 0){
+            printf("Sending message: %s\n", message.message);
+        }else{
+            printf("End of input file! exit!\n");
+        }
+        
+
+    }
+    printf("Total sending time: %lf s\n", time_taken);
+
+
+    sem_close(sem_send);
+    sem_close(sem_recv);
+
+}
